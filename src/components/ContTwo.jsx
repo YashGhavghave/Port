ff98e8f2890e9f1ff8d4668eb68bea1b777005aa
Yash_G.G <157970758+YashGhavghave@users.noreply.github.com>
2024-12-08 20:38:50 +0530

    Update ContTwo.jsx

--- src/components/ContTwo.jsx
+++ src/components/ContTwo.jsx
@@ -3,7 +3,7 @@ import './Conttwo.css'
 
 function Conttwo() {
   return (
-    <div id='container2' className='h-[80vh] w-[80vw] relative top-[82rem] bg-gradient-to-r from-purple-500 to-black  left-[10vw] rounded-3xl max-md:h-[50vh] '>
+    <div id='container2' className='h-[80vh] w-[80vw] relative top-[82rem] bg-gradient-to-r from-purple-500 to-black  left-[10vw] rounded-3xl max-md:h-[70vh] '>
                <p className='text-4xl bg-transparent relative left-[5%] top-[5%] font-bold max-sm:top-[2%]'>@Skills</p>
           <div className='bg-transparent font-mono relative top-[15%] text-3xl max-md:text-xl max-md:top-[10%] text-white'>
             <h2 className='bg-transparent font-mono relative left-[10vw]'>• JavaScript</h2>
