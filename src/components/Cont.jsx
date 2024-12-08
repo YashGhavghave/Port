0a8cbce641ccc73dd7702a36dd6286c1fbf6d2fd
Yash_G.G <157970758+YashGhavghave@users.noreply.github.com>
2024-12-08 20:41:28 +0530

    Update Cont.jsx

--- src/components/Cont.jsx
+++ src/components/Cont.jsx
@@ -3,7 +3,7 @@ import './Cont.css'
 
 function Home() {
   return (
-    <div id='container1' className='h-[80vh] w-[80vw] relative top-[68rem] bg-gradient-to-r from-red-500 to-blue-500  left-[10vw] rounded-3xl max-md:h-[100vh] max-sm:h-[70vh]  max-2xl:h-[90vh]'>
+    <div id='container1' className='h-[80vh] w-[80vw] relative top-[68rem] bg-gradient-to-r from-red-500 to-blue-500  left-[10vw] rounded-3xl max-md:h-[90vh] max-sm:h-[80vh]  max-2xl:h-[90vh]'>
       <p className='text-4xl bg-transparent relative left-[5%] top-[5%] font-bold max-sm:top-[2%]'>@About</p>
       <p className='text-3xl bg-transparent font-mono w-[80%] relative left-[10%] top-[15%] max-lg:text-lg max-lg:top-[10%] max-2xl:text-2xl max-sm:top-[6%] max-sm:text-sm '>
        I'm Yash Ghavghave,  Engineering student pursuing B.Tech in Artificial Intelligence and Machine Learning (AIML) from the FEAT, Sawangi(Meghe), Wardha.
