d52fe52be3127115afe558584c5bdf6477f53785
Yash_G.G <yashghavghave123@gmail.com>
2024-12-09 13:32:11 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import Profile from './components/Profile'
 import Icon from './components/Icon'
 import Home from './components/Cont'
@@ -6,32 +5,12 @@ import TypingEffect from './components/TypingEffect'
 import Conttwo from './components/ContTwo'
 import Intro from './components/intro'
 import Containerthree from './components/Containerthree'
-import axios from 'axios'
 
 function App() {
-  const [fileSrc, setFileSrc] = useState(null);
-
-    useEffect(()=>{
-      const fetchFile = async()=>{
-        try{
-          const responce = await axios.get('http://localhost:3000/Public/image.png', {responseType: 'bolb'})
-          const fileURL = URL.createObjectURL(responce.data)
-          setFileSrc(fileURL);
-        }catch(error){
-          console.error(error)
-        }
-      }
-      fetchFile();
-    })
-
   return (
     <div>
       <Icon/>
-      <Profile>{fileSrc ? (
-                <img src={fileSrc} alt="Dynamic File"/>
-            ) : (
-                <p>Loading file...</p>
-            )}</Profile>
+      <Profile/>
       <TypingEffect/>
       <Home/>
       <Conttwo/>
